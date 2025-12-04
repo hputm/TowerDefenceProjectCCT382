c89ea49450838d120f82bdaa8b42b534c1866daa
hputm <satisfyiinq@gmail.com>
2025-12-04 10:23:03 -0500

    progress in adding scripting

--- Assets/New Scripts/Bullet.cs
+++ Assets/New Scripts/Bullet.cs
@@ -40,9 +40,10 @@ public class Bullet : MonoBehaviour
     }
     void HitTarget ()
     {
-        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform,rotation);
+        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
         Destroy(effectIns, 2f);
 
+        Destroy(target.gameObject);
         Destroy(gameObject);
     }
 }
